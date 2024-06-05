1487212e418cbb4b161ff6e207442298264cb5c7
Ujjawal <ambikastore.uk@gmail.com>
2024-06-06 00:13:51 +0530

    footer-responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-indigo-900 flex py-12 justify-center text-white">
-      <div className="max-w-screen-xl  flex justify-center w-full ">
-        <div className="flex justify-between w-full  ">
+    <footer className="bg-indigo-900 flex py-12 justify-center items-center text-white">
+      <div className="max-w-screen-xl   flex justify-center items-center px-4 w-full ">
+        <div className="flex-col flex md:flex-row justify-between items-center md:items-start  w-full  ">
           <div className="text-3xl">Hospital</div>
-          <div>
-            <ul>
+          <div className="my-4">
+            <ul className="">
               <li className="hover:text-indigo-200 my-3  transition-all">
                 <Link to="/about-us">About</Link>
               </li>
@@ -24,7 +24,7 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="flex">
+          <div className="flex mt-4">
             <div className="mx-2 cursor-pointer hover:text-indigo-300 transition-all"> 
               <CiFacebook size={30} />
             </div>
@@ -36,16 +36,17 @@ function Footer() {
             </div>
           </div>
           <div >
-            <p className="mb-8 text-xl font-bold tracking-wider	">Signup for our newsletter</p>
-            <div>
+            <p className="mb-4 text-md text-center tracking-wider 	">Signup for our newsletter</p>
+            <div className="flex">
               <input
                 type="text"
-                className="bg-transparent rounded-3xl   outline-none px-6 py-1  border-indigo-300 border-2 border-solid "
+                className="bg-transparent rounded-3xl w-48  outline-none px-2 py-1  border-indigo-300   border-2 border-solid "
                 placeholder="Type email here"
               />
-              <button className="ml-2 bg-indigo-100 px-3 py-1 rounded font-semibold text-indigo-700">Submit</button>
+              <button className="ml-2  bg-indigo-100 px-2 py-1 text-sm rounded-3xl font-semibold text-indigo-700">Submit</button>
             </div>
           </div>
+          
         </div>
       </div>
     </footer>
