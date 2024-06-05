3eea5e1645f81f559a6997795c3b3b4460a65504
vinitsingh-13 <singhvinit981@gmail.com>
2024-06-05 21:28:50 +0530

    tailwind issue fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 function Navbar() {
-  return <nav>Navabar</div>;
+  return (
+    <nav className="">
+      <ul className="flex ">
+        <li className="text-red-500">Home</li>
+        <li className="text-red-500">About Us</li>
+        <li className="text-red-500">Testimonals</li>
+        <li className="text-red-500">Contact Us</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
